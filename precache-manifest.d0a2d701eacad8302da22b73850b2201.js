e392481b95af88ba32c90aaee159039ed03c43c4
porosh2002 <jamilkashem2002@gmail.com>
2021-02-04 22:42:16 +0600

    Updates

---
+++ precache-manifest.d0a2d701eacad8302da22b73850b2201.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ad66380d57884c8099f4797edd7bb6",
+    "url": "/Preview/index.html"
+  },
+  {
+    "revision": "b2562fe84966b4859321",
+    "url": "/Preview/static/css/main.81681690.chunk.css"
+  },
+  {
+    "revision": "da43e7d43ecd5f9cfa74",
+    "url": "/Preview/static/js/2.9c451dc0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Preview/static/js/2.9c451dc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4138884b9a07fef48a3",
+    "url": "/Preview/static/js/3.b49bde84.chunk.js"
+  },
+  {
+    "revision": "c67ea08ad9f0f28b7a65",
+    "url": "/Preview/static/js/4.c20bb08b.chunk.js"
+  },
+  {
+    "revision": "91e0e517980ef805f677",
+    "url": "/Preview/static/js/5.f8d3c6eb.chunk.js"
+  },
+  {
+    "revision": "8e7fa176b006150306288bd092a696c0",
+    "url": "/Preview/static/js/5.f8d3c6eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b804980128a6ec95487",
+    "url": "/Preview/static/js/6.3a09a3bd.chunk.js"
+  },
+  {
+    "revision": "a7ea4a1fa2ef392f9a3b",
+    "url": "/Preview/static/js/7.0f63e3fa.chunk.js"
+  },
+  {
+    "revision": "b2562fe84966b4859321",
+    "url": "/Preview/static/js/main.a85b8271.chunk.js"
+  },
+  {
+    "revision": "abc8f42622c1011735c3",
+    "url": "/Preview/static/js/runtime-main.07a62edb.js"
+  },
+  {
+    "revision": "d01731eecbeb065d79d807dacb623a93",
+    "url": "/Preview/static/media/bg-pattern.d01731ee.png"
+  },
+  {
+    "revision": "ebafb27e44acd9a942bae9302541a2d2",
+    "url": "/Preview/static/media/bg_sports_default.ebafb27e.jpg"
+  },
+  {
+    "revision": "1e9cb6717f03eaf8f3087e62d08d5a9e",
+    "url": "/Preview/static/media/error.1e9cb671.svg"
+  },
+  {
+    "revision": "d244b5e9644dbee122c81924cdaa2e4d",
+    "url": "/Preview/static/media/ss.d244b5e9.png"
+  }
+]);
